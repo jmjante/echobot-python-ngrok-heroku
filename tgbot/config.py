06ad6fa6e6ed0b75a4be25032c9f69a304c4c857
jmjante <jmjante@gmail.com>
2019-09-08 22:10:24 +0800

    update config.py - removed assigned
    removed for public viewing

--- tgbot/config.py
+++ tgbot/config.py
@@ -1,5 +1,5 @@
 mode = "prod"
-bot_token = "936401514:AAEqcrYA_IFdzSEWRZhNN42Wo5RBVvhVUms"
-bot_user_name = '@jmjtest_bot'
-HEROKU_APP_NAME = "echobot-python-1"
+bot_token = "TOKEN"
+bot_user_name = '@botusername'
+HEROKU_APP_NAME = "herokuappname"
 NGROK_APP_NAME = "000c4b80" #this should be changed everytime you triger ./ngrok http 80 get the link with https
